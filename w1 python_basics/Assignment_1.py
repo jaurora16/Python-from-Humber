48f6de82b2a031d85e20cc24ca31571e8e8364af
Jessica Aurora <jess.aurora16@gmail.com>
2025-06-30 17:00:08 -0400

    Create Assignment_1.py
    arrays and element-wise sums

---
+++ w1 python_basics/Assignment_1.py
@@ -0,0 +1,12 @@
+# First Name: Jessica
+# Last Name: Aurora
+def get_array_sums(arr1, arr2):
+    #Returns the element-wise sum of two arrays.
+    return [x + y for x, y in zip(arr1, arr2)]
+# Define the arrays
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [6, 7, 8, 9, 10]
+# Calculate the element-wise sums
+result = get_array_sums(arr1, arr2)
+# Print the result
+print(f"The sum of the arrays is: {sum(result)}")
