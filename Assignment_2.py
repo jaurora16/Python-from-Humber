91a2230e07e995749e47eb293adf5dced7f4d907
Jessica Aurora <jess.aurora16@gmail.com>
2025-06-30 16:54:33 -0400

    Delete Assignment_2.py

--- Assignment_2.py
+++
@@ -1,6 +0,0 @@
-# First Name: Jessica
-# Last Name: Aurora
-def remove_duplicates(input_list):
-    return list(dict.fromkeys(input_list))
-input_list = ['Johannes', 'Jamal', 'Jamal', 'Johannes', 'Galina']
-print(*remove_duplicates(input_list))
