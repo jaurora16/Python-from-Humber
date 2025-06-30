a0b2f06389c54039280fc09426e9786ee6098b28
Jessica Aurora <jess.aurora16@gmail.com>
2025-06-30 16:47:43 -0400

    Add files via upload

---
+++ Assignment_0.py
@@ -0,0 +1,14 @@
+# First Name: Jessica
+# Last Name: Aurora
+def get_full_name_length(first_name, last_name):
+    full_name = first_name + last_name
+    return len(full_name)
+# Prompt the user for their first and last names
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+# Calculate the length of the full name
+length = get_full_name_length(first_name, last_name)
+# Display the result
+print(f"The length of your name is {length} characters.")
+#check50 mycode-cloud/predictive/main/1_python_basics/Assignment_0.py
+#submit50 mycode-cloud/predictive/main/1_python_basics/Assignment_0.py
